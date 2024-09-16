b91c4c8c9c491f253a839ae6a371296f5b034a28
tiziano777 <tizianofinizzi@gmail.com>
2024-09-16 14:18:18 +0200

    dim. reduction lenet

--- architecture.py
+++ architecture.py
@@ -1,7 +1,6 @@
 import torch
 from torchvision import models
 
-
 mnas05= models.mnasnet0_5(pretrained=False)
 mnas075= models.mnasnet0_75(pretrained=False)
 regnet_y_400mf = models.regnet_y_400mf(pretrained=False)
@@ -10,7 +9,10 @@ densenet121 = models.densenet121(pretrained=False)
 resnet18 = models.resnet18(pretrained=False)
 lenet = models.googlenet(pretrained=False)
 
-model=densenet121
+model=lenet
+model.aux2.fc2 = torch.nn.Linear(1024, 10, bias=False)
+model.aux1.fc2 = torch.nn.Linear(1024, 10, bias=False)
+model.fc = torch.nn.Linear(10, 10, bias=False)
 
 # Stampa la struttura del modello
 for name, param in model.named_parameters():
